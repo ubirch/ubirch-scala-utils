2b9c79eeb5c18513e84403591a13f86a0f6f2684
cvandrei <crille@indarium.de>
2017-04-06 12:11:29 +0200

    UBI-95: optimized imports

--- mongo-test-utils/src/main/scala/com/ubirch/util/mongo/test/MongoTestUtils.scala
+++ mongo-test-utils/src/main/scala/com/ubirch/util/mongo/test/MongoTestUtils.scala
@@ -1,14 +1,13 @@
 package com.ubirch.util.mongo.test
 
 import com.ubirch.util.mongo.connection.MongoUtil
-import com.ubirch.util.mongo.format.MongoFormats
 
 import reactivemongo.api.commands.bson.BSONCountCommand.Count
 import reactivemongo.api.commands.bson.BSONCountCommandImplicits._
 import reactivemongo.bson.BSONDocument
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /**
   * author: cvandrei
