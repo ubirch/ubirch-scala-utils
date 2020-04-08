961a071c09277642bc00f733a69f86d4e0d8ba59
lotta <lotta.rueger@ubirch.com>
2020-04-08 15:29:23 +0200

    changing some validations steps only to become executed when invalid Signatures are not allowed writing the error handling a bit more concise upgrading a version number of oidc-utils

--- oidc-utils/src/main/scala/com/ubirch/util/oidc/model/UserContext.scala
+++ oidc-utils/src/main/scala/com/ubirch/util/oidc/model/UserContext.scala
@@ -1,5 +1,7 @@
 package com.ubirch.util.oidc.model
 
+import java.util.UUID
+
 /**
   * author: cvandrei
   * since: 2017-03-23
@@ -7,7 +9,7 @@ package com.ubirch.util.oidc.model
 case class UserContext(context: String,
                        providerId: String,
                        externalUserId: String,
-                       userId: String,
+                       userId: UUID,
                        userName: String,
                        locale: String,
                        email: Option[String] = None,
